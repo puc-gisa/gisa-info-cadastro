5dc3ef8968398040c8462a6cdc44e7f297fcceac
Alcides <1287814@sga.pucminas.br>
2023-03-21 14:47:25 -0300

    add: message using rabbitMQ

---
+++ src/main/java/br/com/gisa/gisainfocadastro/config/Amqp.java
@@ -0,0 +1,11 @@
+package br.com.gisa.gisainfocadastro.config;
+
+public class Amqp {
+
+    private Amqp() {
+    }
+
+    public static final String NOVO_ASSOCIADO_EXCHANGE = "associado.novo";
+    public static final String ATUALIZADO_ASSOCIADO_EXCHANGE = "associado.atualizado";
+
+}
